7e9b0be4cd790888354454970201d4c4dc9fd804
felcg <xfelipecg@gmail.com>
2022-09-15 21:11:43 -0300

    chore: initial configuration of project
    add craco, react-router-dom, sass, commitizen packages. Add aliases, jest configs, router and home
    page

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.paths.json");
+
+module.exports = {
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.{ts,tsx}",
+    "!<rootDir>/src/**/index.{ts,tsx}",
+    "!<rootDir>/src/index.{ts,tsx}",
+    "!<rootDir>/src/**/index.ts",
+    "!**/*.d.ts",
+  ],
+  coverageDirectory: "coverage",
+  setupFilesAfterEnv: ["<rootDir>/src/main/config/setupTests.ts"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  testEnvironment: "jsdom",
+  transform: {
+    ".+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/tests/mocks/mock-image.js",
+    ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
+    ...pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: "<rootDir>/src",
+    }),
+  },
+};
