0f00fe70080052430985eaf0877191e34c96263c
felcg <xfelipecg@gmail.com>
2022-09-15 21:51:07 -0300

    feat(infra): create object with pokemonTgcApi urls

---
+++ src/infra/protocols/pokemonTgcApi/urls.ts
@@ -0,0 +1,10 @@
+const baseUrl = 'https://api.pokemontcg.io/v2/'
+
+export const pokemonApi = {
+  cards: `${baseUrl}cards`,
+  sets: `${baseUrl}sets`,
+  types: `${baseUrl}types`,
+  subtypes: `${baseUrl}subtypes`,
+  supertypes: `${baseUrl}supertypes`,
+  rarities: `${baseUrl}rarities`,
+}
