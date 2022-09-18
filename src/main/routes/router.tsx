2636301563fbc78894a073c2d9a889c4867e4786
felcg <xfelipecg@gmail.com>
2022-09-18 20:18:43 -0300

    feat(main): add CardDetails route

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
-import { Home } from "@presentation/pages";
+import { CardDetails, Home } from "@presentation/pages";
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<CardDetails />} />
       </Routes>
     </BrowserRouter>
   );
