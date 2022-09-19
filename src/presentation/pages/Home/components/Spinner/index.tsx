ba1f6ad9dbda11bccdfe81a362f7d9abe3d8a1ac
felcg <xfelipecg@gmail.com>
2022-09-18 21:19:01 -0300

    feat(presentation): add Spinner component

---
+++ src/presentation/pages/Home/components/Spinner/index.tsx
@@ -0,0 +1,7 @@
+import "./spinner-styles.scss";
+
+const Spinner: React.FC = () => {
+  return <div className="loader"></div>;
+};
+
+export default Spinner;
--- src/presentation/pages/Home/components/index.ts
+++ src/presentation/pages/Home/components/index.ts
@@ -3,5 +3,6 @@ export { default as Logo } from './Logo'
 export { default as PokeCard } from './PokeCard'
 export { default as Carousel } from './Swiper'
 export { default as CardsContainer } from './CardsContainer'
+export { default as Spinner } from './Spinner'
 
 
