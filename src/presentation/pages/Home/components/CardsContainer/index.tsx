85f8cf64e5b8a0663b97f05ffa4b9324d469b34f
felcg <xfelipecg@gmail.com>
2022-09-18 21:19:30 -0300

    feat(presentation): show spinner while loading cards

--- src/presentation/pages/Home/components/CardsContainer/index.tsx
+++ src/presentation/pages/Home/components/CardsContainer/index.tsx
@@ -1,15 +1,25 @@
 import PokeCard from "../PokeCard";
 import { CardsContainerProps } from "@presentation/protocols";
 import "./styles.scss";
+import { useContext } from "react";
+import { CardsContext } from "@presentation/context";
+import { Spinner } from "@presentation/pages/Home/components";
 
 const CardsContainer: React.FC<CardsContainerProps> = (props) => {
   const { cards } = props;
+  const { loading } = useContext(CardsContext);
   return (
-    <div className="cardsContainer">
-      {cards &&
-        cards.map((card) => {
-          return <PokeCard card={card} />;
-        })}
+    <div className="container">
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="cardsContainer">
+          {cards &&
+            cards.map((card) => {
+              return <PokeCard card={card} />;
+            })}
+        </div>
+      )}
     </div>
   );
 };
--- src/presentation/pages/Home/components/Swiper/index.tsx
+++ src/presentation/pages/Home/components/Swiper/index.tsx
@@ -2,29 +2,40 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 import PokeCard from "../PokeCard";
 import { CarouselProps } from "@presentation/protocols";
+import { useContext } from "react";
+import { CardsContext } from "@presentation/context";
+import { Spinner } from "@presentation/pages/Home/components";
 import "swiper/css";
 import "swiper/css/navigation";
 import "./styles.scss";
 
 const Carousel: React.FC<CarouselProps> = (props) => {
   const { setSwiperInstance, cards } = props;
+  const { loading } = useContext(CardsContext);
+
   return (
-    <Swiper
-      modules={[Navigation]}
-      navigation
-      spaceBetween={50}
-      slidesPerView={1}
-      initialSlide={0}
-      onSwiper={(swiper) => setSwiperInstance(swiper)}
-    >
-      {cards?.map((card) => {
-        return (
-          <SwiperSlide key={card.id}>
-            <PokeCard card={card} />
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Swiper
+          modules={[Navigation]}
+          navigation
+          spaceBetween={50}
+          slidesPerView={1}
+          initialSlide={0}
+          onSwiper={(swiper) => setSwiperInstance(swiper)}
+        >
+          {cards?.map((card) => {
+            return (
+              <SwiperSlide key={card.id}>
+                <PokeCard card={card} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      )}
+    </>
   );
 };
 
